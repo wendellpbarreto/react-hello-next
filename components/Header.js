73f3db888ac2976e557ac52ed8cfca534493e58a
Wendell P. Barreto <wendell@tootz.com.br>
2019-08-18 19:13:41 -0300

    Add posts page as dynamic routing.

--- components/Header.js
+++ components/Header.js
@@ -12,6 +12,9 @@ const Header = () => (
         <Link href="/about">
             <a style={linkStyle}>About</a>
         </Link>
+        <Link href="/blog">
+            <a style={linkStyle}>Blog</a>
+        </Link>
     </div>
 );
 
---
+++ pages/blog.js
@@ -0,0 +1,25 @@
+import withLayout from '../components/MyLayout';
+import Link from 'next/link';
+
+const PostLink = props => (
+    <li>
+        <Link href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+);
+
+const Blog = () => {
+    return (
+        <div>
+            <h1>My Blog</h1>
+            <ul>
+                <PostLink title="Hello Next.js" />
+                <PostLink title="Learn Next.js is awesome" />
+                <PostLink title="Deploy apps with Zeit" />
+            </ul>
+        </div>
+    );
+}
+
+export default withLayout(Blog)
