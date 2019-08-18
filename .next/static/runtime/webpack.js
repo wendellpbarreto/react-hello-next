d4d546c5e0c48d7c42ff26a6baf3200e9051506a
Wendell P. Barreto <wendell@tootz.com.br>
2019-08-18 18:46:16 -0300

    Add link to navigate between pages.

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "526940f4ad553e9fcaad";
+/******/ 	var hotCurrentHash = "40bac9f7e65da9b9ad94";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
